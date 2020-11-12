3ebbb41274feff647e500ea1de3155caa787059a
afw <71993863+alissafwoods@users.noreply.github.com>
2020-11-11 22:04:30 -0800

    Updated column/row formatting for hourly forecast

--- src/script.js
+++ src/script.js
@@ -138,7 +138,7 @@ function displayForecast(response) {
 
   for (let index = 0; index < 6; index++) {
     forecast = response.data.list[index];
-    forecastElement.innerHTML += `<div class="col" id="forecast">
+    forecastElement.innerHTML += `<div class="col-2">
             <div class="card">
               <div class="card-body">
                 <p class="time">${formatHours(forecast.dt * 1000)}</p>
