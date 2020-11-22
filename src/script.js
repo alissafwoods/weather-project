0dfd8fa044b5cb28fd8818688fc82086794d5d1c
afw <71993863+alissafwoods@users.noreply.github.com>
2020-11-22 10:02:46 -0800

    Fixed hourly forecast for current location button

--- src/script.js
+++ src/script.js
@@ -160,6 +160,9 @@ function showCurrentLocationTemperature(position) {
   let units = "metric";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
   axios.get(`${apiUrl}`).then(currentWeather);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(displayForecast);
 }
 
 function getCurrentPosition() {
